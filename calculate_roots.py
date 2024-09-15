fb0cc2cb88c6541534c1732f378523c5d41d980a
Kevins-repos <kevalcantara2005@gmail.com>
2024-09-15 10:41:43 -0500

    unit 4 done

--- calculate_roots.py
+++ calculate_roots.py
@@ -6,17 +6,30 @@
 # Section:      578
 # Assignment:   4.21 LAB: Calculate roots
 # Date:         13 9 2024
+#too much math
 import math
-a = int(input('Please enter the coefficient A: '))
-b = int(input('Please enter the coefficient B: '))
-c = int(input('Please enter the coefficient C: '))
-#fancy try-except method cause I just learned what it does and how to use it
-try:
-    p1 = -b/(2*a)
-    p2 = math.sqrt(b**2-4*a*c)/(2*a)
-    if(p1-p2 == p1+p2):
-        print(f'The root is x = {p1-p2}')
-    else:
-        print(f'The roots are x = {p1+p2} and x = {p1-p2}')
-except ZeroDivisionError:
-    print('You entered an invalid combination of coefficients!')
+a = float(input("Please enter the coefficient A: "))
+b = float(input("Please enter the coefficient B: "))
+c = float(input("Please enter the coefficient C: "))
+
+if a == 0 and b == 0:
+    print("You entered an invalid combination of coefficients!")
+    quit()
+
+if a == 0:
+    p1 = -c / b
+    print(f"The root is x = {p1}")
+    quit()
+
+p2 = b**2 - 4*a*c
+if p2 > 0:
+    x1 = (-b + math.sqrt(p2)) / (2*a)
+    x2 = (-b - math.sqrt(p2)) / (2*a)
+    print(f"The roots are x = {x1} and x = {x2}")
+elif p2 == 0:
+    p1 = -b / (2*a)
+    print(f"The root is x = {p1}")
+else:
+    real_part = -b / (2*a)
+    imaginary_part = math.sqrt(-p2) / (2*a)
+    print(f"The roots are x = {real_part} + {imaginary_part}i and x = {real_part} - {imaginary_part}i")
--- notes.py
+++
@@ -1,4 +0,0 @@
-import math
-s = math.pi
-print(f'this is how you do it {s} cut off as {s:.3}')
-print(11//10 + 10)
