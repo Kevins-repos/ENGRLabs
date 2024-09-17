f876d5f865e80ac13697e3063490272259a51d21
TasteDeRambo <anonimous3746@gmail.com>
2024-09-17 18:20:17 -0500

    unit 5 group

--- tyr_test_cases.py
+++ tyr_test_cases.py
@@ -1,34 +1,87 @@
-sex = input('Male(M) or Female(F): ')
-#string type
-#idk why sex!="M" is always wrong(same for F) ill just use not
-if(not(sex=="M" or sex=="F")):
-    print("Please enter either M or F")
-    quit()
-age = int(input('Age :'))
-#check if age is within range
-if(age>79 or age<20):
-    print('Please enter an age between 20 and 79, inclusive')
-    quit()
-#redundancy function
-def AgePointScore(age, point):
-    threshold = 79
-    x = 0
-    while(age<=threshold):
-        #decrease threshold to the lower range
-        threshold-=5
-        #as range lowers, points change accordingly 
-        x+=1
-        #last range is special since its a larger range
-        if(age<=34 and age>=20):
-            x-=1
-    #arrays start from 0 and go to 10 even if only 9 elements
-    return point[x-1]
-#set up points per age range backwards as we start with oldest range first
-Mpoints = [13, 12, 11, 10, 8, 6, 3, 0, -4, -9]
-Fpoints = [16, 14, 12, 10, 8, 6, 3, 0, -3, -7]
-#decide to use either male or female point sets
-if(sex=="M"):
-    print(AgePointScore(age, Mpoints))
-    #quit early to avoid running female points too
-    quit()
-print(AgePointScore(age, Fpoints))
+sex:F age:20 cho:100 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:F age:20 cho:160 smo:N hdl:50 sbp:119 med:Y out:<1
+sex:F age:20 cho:200 smo:Y hdl:59 sbp:120 med:N out:1
+sex:F age:39 cho:240 smo:Y hdl:40 sbp:120 med:Y out:<1
+sex:F age:39 cho:280 smo:N hdl:49 sbp:130 med:N out:1
+
+sex:F age:40 cho:100 smo:Y hdl:39 sbp:130 med:Y out:2
+sex:F age:40 cho:160 smo:N hdl:60 sbp:140 med:N out:<1
+sex:F age:40 cho:200 smo:Y hdl:60 sbp:140 med:Y out:<1
+sex:F age:49 cho:240 smo:Y hdl:60 sbp:160 med:N out:<1
+sex:F age:49 cho:280 smo:N hdl:60 sbp:160 med:Y out:<1
+
+sex:F age:50 cho:100 smo:Y hdl:60 sbp:129 med:N out:<1
+sex:F age:50 cho:160 smo:N hdl:60 sbp:129 med:Y out:<1
+sex:F age:50 cho:200 smo:Y hdl:60 sbp:139 med:N out:<1
+sex:F age:59 cho:240 smo:Y hdl:60 sbp:139 med:Y out:<1
+sex:F age:59 cho:280 smo:N hdl:60 sbp:159 med:N out:<1
+
+sex:F age:60 cho:100 smo:Y hdl:60 sbp:159 med:Y out:<1
+sex:F age:60 cho:160 smo:N hdl:60 sbp:161 med:N out:<1
+sex:F age:60 cho:200 smo:Y hdl:60 sbp:161 med:Y out:<1
+sex:F age:69 cho:240 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:F age:69 cho:280 smo:N hdl:60 sbp:100 med:N out:<1
+
+sex:F age:70 cho:100 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:F age:70 cho:160 smo:N hdl:60 sbp:100 med:N out:<1
+sex:F age:70 cho:200 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:F age:79 cho:240 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:F age:79 cho:280 smo:N hdl:60 sbp:100 med:N out:<1
+
+sex:F age:2 cho:100 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:F age:2 cho:100 smo:N hdl:60 sbp:119 med:N out:<1
+sex:F age:80 cho:290 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:F age:80 cho:290 smo:N hdl:60 sbp:119 med:N out:<1
+
+sex:F age:39 cho:160 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:F age:49 cho:200 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:F age:59 cho:240 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:F age:69 cho:280 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:F age:79 cho:290 smo:Y hdl:60 sbp:119 med:N out:<1
+
+
+
+##MALE
+sex:M age:20 cho:100 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:M age:20 cho:160 smo:N hdl:50 sbp:119 med:Y out:<1
+sex:M age:20 cho:200 smo:Y hdl:59 sbp:120 med:N out:<1
+sex:M age:39 cho:240 smo:Y hdl:40 sbp:120 med:Y out:<1
+sex:M age:39 cho:280 smo:N hdl:49 sbp:130 med:N out:<1
+
+sex:M age:40 cho:100 smo:Y hdl:39 sbp:130 med:Y out:<1
+sex:M age:40 cho:160 smo:N hdl:60 sbp:140 med:N out:<1
+sex:M age:40 cho:200 smo:Y hdl:60 sbp:140 med:Y out:<1
+sex:M age:49 cho:240 smo:Y hdl:60 sbp:160 med:N out:<1
+sex:M age:49 cho:280 smo:N hdl:60 sbp:160 med:Y out:<1
+
+sex:M age:50 cho:100 smo:Y hdl:60 sbp:129 med:N out:<1
+sex:M age:50 cho:160 smo:N hdl:60 sbp:129 med:Y out:<1
+sex:M age:50 cho:200 smo:Y hdl:60 sbp:139 med:N out:<1
+sex:M age:59 cho:240 smo:Y hdl:60 sbp:139 med:Y out:<1
+sex:M age:59 cho:280 smo:N hdl:60 sbp:159 med:N out:<1
+
+sex:M age:60 cho:100 smo:Y hdl:60 sbp:159 med:Y out:<1
+sex:M age:60 cho:160 smo:N hdl:60 sbp:161 med:N out:<1
+sex:M age:60 cho:200 smo:Y hdl:60 sbp:161 med:Y out:<1
+sex:M age:69 cho:240 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:69 cho:280 smo:N hdl:60 sbp:100 med:N out:<1
+
+sex:M age:70 cho:100 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:70 cho:160 smo:N hdl:60 sbp:100 med:N out:<1
+sex:M age:70 cho:200 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:79 cho:240 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:79 cho:280 smo:N hdl:60 sbp:100 med:N out:<1
+
+sex:M age:2 cho:240 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:2 cho:240 smo:N hdl:60 sbp:100 med:N out:<1
+sex:M age:80 cho:280 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:80 cho:280 smo:N hdl:60 sbp:100 med:N out:<1
+
+sex:M age:39 cho:100 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:39 cho:160 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:39 cho:200 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:39 cho:280 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:49 cho:200 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:M age:59 cho:240 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:M age:69 cho:280 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:M age:79 cho:290 smo:Y hdl:60 sbp:119 med:N out:<1
