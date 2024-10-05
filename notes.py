22a633699bd0b679efb19f011949de3d99a3970b
TasteDeRambo <anonimous3746@gmail.com>
2024-10-05 17:04:32 -0500

    unit 7 individual done

--- notes.py
+++ notes.py
@@ -1,8 +1,20 @@
-begin = '\x1b[1m'
-end = '\x1b[39m\x1b[22m'
-red = '\x1b[31m'
-blue = '\x1b[34m'
-green = '\x1b[32m'
-print(begin + red + 'red text' + end + ' back to normal')
-print(begin + blue + 'blue text' + end + ' back to normal')
-print(begin + green + 'green text' + end + ' back to normal')
+def kaprekars_constant(num):
+    if 0 < num < 9999:
+        while num != 6174:
+            ascending, descending = sort_nums(num)
+            num = descending - ascending
+            print(f"Next number: {num}")
+
+def sort_nums(num):
+    nums = [int(i) for i in str(num)]
+    while len(nums) < 4:
+        nums.append(0)
+    nums.sort()
+    ascending = int("".join(map(str, nums)))
+    nums.sort(reverse=True)
+    descending = int("".join(map(str, nums)))
+    print(ascending, descending)
+    return ascending, descending
+
+num = int(input('Enter a four-digit integer: '))
+kaprekars_constant(num)
