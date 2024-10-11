7413c3a00c867449681ce8860453f6ef59ae967e
TasteDeRambo <anonimous3746@gmail.com>
2024-10-10 19:46:05 -0500

    my part of unit 8 done

--- ascii_clock.py
+++ ascii_clock.py
@@ -1,9 +1,12 @@
-nums = ['***\n* *\n* *\n* *\n***\n', ' * \n** \n * \n * \n***\n', '***\n  *\n***\n*\n***\n',
-        '***\n  *\n***\n  *\n***\n', '* *\n* *\n***\n  *\n  *\n', '***\n*\n***\n  *\n***\n', 
-        '***\n*\n***\n* *\n***\n', '***\n  *\n  *\n  *\n  *\n', '***\n* *\n***\n* *\n***\n',
-        '***\n* *\n***\n  *\n  *\n']
-symbol = input('Input:')
-for i in nums:
-    for j in i:
-        if j!=symbol:
-            j = nums
+def acsiiNums(symbol: str):
+    """Enter a symbol and returns a list with all acsii numbers in that symbol"""
+    nums = ['***\n* *\n* *\n* *\n***\n', ' * \n** \n * \n * \n***\n', '***\n  *\n***\n*\n***\n',
+            '***\n  *\n***\n  *\n***\n', '* *\n* *\n***\n  *\n  *\n', '***\n*\n***\n  *\n***\n', 
+            '***\n*\n***\n* *\n***\n', '***\n  *\n  *\n  *\n  *\n', '***\n* *\n***\n* *\n***\n',
+            '***\n* *\n***\n  *\n  *\n']
+    allowed = 'abcdeghkmnopqrsuvwxyz@$&*='
+    while -1==allowed.find(str(symbol)):
+        symbol = input('Character not permitted! Try again: ')
+    for i, char in enumerate(nums):#Minutes[i]=char
+        nums[i] = char.replace('*', symbol)
+    return nums
--- notes.py
+++ notes.py
@@ -1,20 +1,4 @@
-def kaprekars_constant(num):
-    if 0 < num < 9999:
-        while num != 6174:
-            ascending, descending = sort_nums(num)
-            num = descending - ascending
-            print(f"Next number: {num}")
-
-def sort_nums(num):
-    nums = [int(i) for i in str(num)]
-    while len(nums) < 4:
-        nums.append(0)
-    nums.sort()
-    ascending = int("".join(map(str, nums)))
-    nums.sort(reverse=True)
-    descending = int("".join(map(str, nums)))
-    print(ascending, descending)
-    return ascending, descending
-
-num = int(input('Enter a four-digit integer: '))
-kaprekars_constant(num)
+age = {"time": [1, 2, 3, 4, 5, 6, 7, 8 , 9, 0],
+       "distance": [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
+for i in age:
+    print(f'key {i} values {age[i][0]}')
