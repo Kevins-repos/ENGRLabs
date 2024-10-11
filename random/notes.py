50a450a51e54308ed04fa3a4865c30a538fb179e
Kevins-repos <kevalcantara2005@gmail.com>
2024-10-10 22:52:11 -0500

    my part of unit 8 fixed

--- random/notes.py
+++ random/notes.py
@@ -1,4 +1,25 @@
-age = {"time": [1, 2, 3, 4, 5, 6, 7, 8 , 9, 0],
-       "distance": [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
-for i in age:
-    print(f'key {i} values {age[i][0]}')
+def asciiTime(symbol: str, hour: int, minute: int):
+    """Enter a symbol and returns a list with all ascii numbers in that symbol"""
+    nums = ['***\n* *\n* *\n* *\n***', ' * \n** \n * \n * \n***', '***\n  *\n***\n*  \n***',
+            '***\n  *\n***\n  *\n***', '* *\n* *\n***\n  *\n  *', '***\n*  \n***\n  *\n***', 
+            '***\n*  \n***\n* *\n***', '***\n  *\n  *\n  *\n  *', '***\n* *\n***\n* *\n***',
+            '***\n* *\n***\n  *\n  *']#make sure all these have the EXACT same spacing
+    allowed = 'abcdeghkmnopqrsuvwxyz@$&*='
+    while str(symbol) not in allowed:#handles numbers too
+        symbol = input('Character not permitted! Try again: ')
+    for i, char in enumerate(nums):
+        nums[i] = char.replace('*', symbol)#char = nums[i], im just making sure 
+    time = [nums[i] for i in [int(i) for i in str(hour)]] + [' \n:\n \n:\n '] + [nums[i] for i in [int(i) for i in str(minute)]]#split into list, turn int into ascii art, combine both lists
+    
+    # Split each digit's ASCII art into lines
+    lines = ['' for _ in range(5)]#create 5 lines to be printee verically
+    for digit in time:#each element in time, as in, each ascii art for each number 
+        digit_lines = digit.split('\n')#separate each line of the individual art, separated by \n, into a list to be printed vertically
+        for i in range(5):
+            lines[i] += digit_lines[i] + '  '  # start adding each number art one by one, line by line, then add some space between arts
+    
+    # Print each line of lines 
+    for line in lines:
+        print(line)
+
+asciiTime('o', 48, 90)
--- unit 8/ascii_clock.py
+++ unit 8/ascii_clock.py
@@ -1,12 +1,19 @@
-def acsiiNums(symbol: str):
-    """Enter a symbol and returns a list with all acsii numbers in that symbol"""
-    nums = ['***\n* *\n* *\n* *\n***\n', ' * \n** \n * \n * \n***\n', '***\n  *\n***\n*\n***\n',
-            '***\n  *\n***\n  *\n***\n', '* *\n* *\n***\n  *\n  *\n', '***\n*\n***\n  *\n***\n', 
-            '***\n*\n***\n* *\n***\n', '***\n  *\n  *\n  *\n  *\n', '***\n* *\n***\n* *\n***\n',
-            '***\n* *\n***\n  *\n  *\n']
+def asciiTime(symbol: str, hour: int, minute: int):
+    """Enter a symbol and returns a list with all ascii numbers in that symbol"""
+    nums = ['***\n* *\n* *\n* *\n***', ' * \n** \n * \n * \n***', '***\n  *\n***\n*  \n***',
+            '***\n  *\n***\n  *\n***', '* *\n* *\n***\n  *\n  *', '***\n*  \n***\n  *\n***', 
+            '***\n*  \n***\n* *\n***', '***\n  *\n  *\n  *\n  *', '***\n* *\n***\n* *\n***',
+            '***\n* *\n***\n  *\n  *']
     allowed = 'abcdeghkmnopqrsuvwxyz@$&*='
-    while -1==allowed.find(str(symbol)):
+    while str(symbol) not in allowed:
         symbol = input('Character not permitted! Try again: ')
-    for i, char in enumerate(nums):#Minutes[i]=char
+    for i, char in enumerate(nums):
         nums[i] = char.replace('*', symbol)
-    return nums
+    time = [nums[i] for i in [int(i) for i in str(hour)]] + [' \n:\n \n:\n '] + [nums[i] for i in [int(i) for i in str(minute)]]
+    lines = ['' for _ in range(5)]
+    for digit in time:
+        digit_lines = digit.split('\n')
+        for i in range(5):
+            lines[i] += digit_lines[i] + '  '
+    for line in lines:
+        print(line)
