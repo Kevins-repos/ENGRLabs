4f7fb22733a2a2a79d05e2f1da0126659a022960
TasteDeRambo <anonimous3746@gmail.com>
2024-10-30 00:04:48 -0500

    cant figure this one out

---
+++ unit 11/weather_data.py
@@ -0,0 +1,11 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Kevin Alcantara
+# Section:      578
+# Assignment:   11.17 LAB: Weather data
+# Date:         29 10 2024
+#comment
+import csv
+
