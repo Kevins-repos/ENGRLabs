0904ae0b789f1a680991c3bb53dc842b4aadb4fb
TasteDeRambo <anonimous3746@gmail.com>
2024-10-31 17:11:03 -0500

    why wont it pass

--- unit 11/passport_checker.py
+++ unit 11/passport_checker.py
@@ -1,33 +1,20 @@
-
-def placeAndSortInList(file):
-    eachPassport = ['']
-    passData = ''
-    index = 0
-    validCount = 0
-    for row in file:
-        if row.replace('\n', '') != '':
-            passData += row
-        else:
-            eachPassport[index] = passData.replace('\n', ' ').split(' ')
-            eachPassport[index].sort()
-            eachPassport[index][0] = 'valid' if len(eachPassport[index])==9 else 'invalid'
-            if eachPassport[index][0] == 'valid':
-                validCount+=1
-            passData = ''
-            index+=1
-            eachPassport.append('')
-    return eachPassport, validCount
 fileName = input('Enter the name of the file: ')
-allPassports = open(fileName, 'r')
+allPassports = open(fileName, 'r').readlines()
 try:
     fileOut = open(file='valid_passports.txt', mode= 'x+')
 except FileExistsError:
     fileOut = open(file='valid_passports.txt', mode= 'r+')
     fileOut.truncate()
-results = placeAndSortInList(allPassports)
-for i in results[0]:
-    if 'valid' in i:
-        fileOut.write(' '.join(i[1:]))
-        fileOut.write('\n')
-print('There are', results[1], 'valid passports')
-allPassports.close(), fileOut.close()
+index, check, count = 0, '', 0
+while index < len(allPassports):
+    for row in allPassports:
+        if row != '\n':
+            check += row
+        else:
+            if 'byr' in check and 'pid' in check and 'iyr' in check and 'cid' in check and 'eyr' in check and 'hcl' in check and 'ecl' in check and 'hgt' in check:
+                fileOut.write(check)
+                fileOut.write('\n')
+                count += 1
+            index += 1
+            check = ''
+print('There are', count, 'valid passports')
