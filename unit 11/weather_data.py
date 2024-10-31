b88e6022f188e9663f041480aacef057416cb138
TasteDeRambo <anonimous3746@gmail.com>
2024-10-31 11:20:15 -0500

    unit 11 individual done

--- unit 11/weather_data.py
+++ unit 11/weather_data.py
@@ -7,10 +7,11 @@
 # Assignment:   11.17 LAB: Weather data
 # Date:         29 10 2024
 #comment
-weather = open('unit 11/WeatherDataCLL-1.csv', 'r')
+weather = open('WeatherDataCLL.csv', 'r')
 usable, maxTemp, minTemp = [], 0, 100
 for row in weather:
     usable.append(row.split(','))
+weather.close()
 for row in usable:
     if row[5] != 'Maximum Temperature (F)' and row[5] != '':
         if float(row[5])>maxTemp:
@@ -24,37 +25,37 @@ print(f'10-year minimum temperature: {round(minTemp)} F\n')
 month = input('Please enter a month: ')
 year = input('Please enter a year: ')
 print(f'For {month} {year}:')
-months = ['', 'January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+months = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 month = months.index(month)
 date = year + '-' + (str(month) if month>9 else '0'+str(month))
-
-dewSum, count = 0, 0
-for row in usable:
-    if date in row[0]:
-        dewSum += float(row[1])
-        count += 1
-print(round(dewSum/count, 1), 'F')
 
 tempSum, count = 0, 0
 for row in usable:
     if date in row[0]:
         tempSum += float(row[2])
         count +=1
-print(round(tempSum/count, 1), 'F')
+print(f'Mean average daily temperature: {tempSum/count:.1f} F')
+
+dewSum, count = 0, 0
+for row in usable:
+    if date in row[0]:
+        dewSum += float(row[1])
+        count += 1
+print(f'Mean average daily dew point: {dewSum/count:.1f} F')
 
 humiditySum, count = 0, 0
 for row in usable:
     if date in row[0]:
         humiditySum += float(row[3])
         count +=1
-print(round(humiditySum/count, 1), '%')
+print(f'Mean relative humidity: {humiditySum/count:.1f} %')
 
 windSum, count = 0, 0
 for row in usable:
     if date in row[0]:
         windSum += float(row[4])
         count +=1
-print(round(windSum/count, 2), 'mph')
+print(f'Mean daily wind speed: {windSum/count:.2f} mph')
 
 total, hasPre = 0, 0
 for row in usable:
@@ -62,4 +63,4 @@ for row in usable:
         total += 1
         if float(row[7]) != 0:
             hasPre += 1
-print(round(hasPre/total*100, 1), '%')
+print(f'Percentage of days with precipitation: {hasPre/total*100:.1f} %')
