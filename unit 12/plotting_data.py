66c5df4fde718425f5162ba67c30eb081ddf1773
TasteDeRambo <anonimous3746@gmail.com>
2024-11-16 22:47:42 -0600

    this assignment...

--- unit 12/plotting_data.py
+++ unit 12/plotting_data.py
@@ -1,3 +1,11 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Kevin Alcantara
+# Section:      578
+# Assignment:   Lab 12 Individual
+# Date:         16 11 2024
 import pandas as pd
 import matplotlib.pyplot as plt
 
