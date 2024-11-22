594f1db6f85d2a98b9e05f93c95070f615901c40
TasteDeRambo <anonimous3746@gmail.com>
2024-11-22 10:49:51 -0600

    almost

--- unit 13/test.py
+++ unit 13/test.py
@@ -18,16 +18,6 @@ def setPlayers() -> dict:
         except ValueError:
             print('Invalid input. Please enter a number between 2 and 4.')
 
-def losePiece(color: str) -> None:
-    global players
-    if len(players[color]) > 0:
-        players[color].pop()
-        print(f'{color.capitalize()} player now has {len(players[color])} pieces left')
-
-        if len(players[color]) == 0:
-            print(f'{color.capitalize()} has been eliminated')
-            players.pop(color)
-
 def nextTurn(current_turn) -> str:
     colors = list(players.keys())
     idx = (colors.index(current_turn) + 1) % len(colors)
@@ -56,6 +46,9 @@ def createBoard():
     board.append(line3.copy())
     board.append(line2.copy())
     board.append(line1.copy())
+    for i in range(1, 16):
+        board[i][8] = '□'
+        board[8][i] = '□'
     return board
 
 def showBoard(board):
@@ -78,96 +71,186 @@ def placePlayers(players, board: list):
             board[placements[color][i][0]][placements[color][i][1]] = placements[color][-1]
 
 # Define the track for each color
-paths = {
-    'red': [(12, 2), (11, 2), (10, 2), (9, 2), (8, 2), (7, 2), (6, 2), (5, 2), (4, 2), (3, 2), (2, 2), 
-            (2, 3), (2, 4), (3, 4), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4), (9, 4), (10, 4), (11, 4), (12, 4)],
-    'blue': [(12, 12), (11, 12), (10, 12), (9, 12), (8, 12), (7, 12), (6, 12), (5, 12), (4, 12), (3, 12), (2, 12), 
-            (2, 13), (2, 14), (3, 14), (4, 14), (5, 14), (6, 14), (7, 14), (8, 14), (9, 14), (10, 14), (11, 14), (12, 14)],
-    'green': [(2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2), (9, 2), (10, 2), (11, 2), (12, 2),
-              (12, 3), (12, 4), (11, 4), (10, 4), (9, 4), (8, 4), (7, 4), (6, 4), (5, 4), (4, 4), (3, 4), (2, 4)],
-    'yellow': [(2, 12), (3, 12), (4, 12), (5, 12), (6, 12), (7, 12), (8, 12), (9, 12), (10, 12), (11, 12), (12, 12),
-               (12, 13), (12, 14), (11, 14), (10, 14), (9, 14), (8, 14), (7, 14), (6, 14), (5, 14), (4, 14), (3, 14), (2, 14)]
+track = [(7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (6, 7), (5, 7), (4, 7), (3, 7), (2, 7), (1, 7), (0, 7), (0, 8), (0, 9), 
+         (1, 9), (2, 9), (3, 9), (4, 9), (5, 9), (6, 9), (7, 10), (7, 11), (7, 12), (7, 13), (7, 14), (7, 15), (7, 16), 
+         (8, 16), (9, 16), (9, 15), (9, 14), (9, 13), (9, 12), (9, 11), (9, 10), (10, 9), (11, 9), (12, 9), (13, 9), 
+         (14, 9), (15, 9), (16, 9), (16, 8), (16, 7), (15, 7), (14, 7), (13, 7), (12, 7), (11, 7), (10, 7), (9, 6), 
+         (9, 5), (9, 4), (9, 3), (9, 2), (9, 1), (9, 0), (8, 0), (7, 0), (7, 1)]
+
+starting_positions = {
+    'green': (7, 1),
+    'yellow': (1, 9),
+    'blue': (9, 15),
+    'red': (15, 7)
 }
+
+# Define home lanes for each color
+home_lanes = {
+    'green': [(8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7)],
+    'red': [(15, 8), (14, 8), (13, 8), (12, 8), (11, 8), (10, 8), (9, 8)],
+    'blue': [(8, 15), (8, 14), (8, 13), (8, 12), (8, 11), (8, 10), (8, 9)],
+    'yellow': [(1, 8), (2, 8), (3, 8), (4, 8), (5, 8), (6, 8), (7, 8)]
+}
+
+# Create the board with home lanes
+def createBoard():
+    line3 = ['O', '.', 'O', '.', 'O', '.', 'O', '.', '.', '.', 'O', '.', 'O', '.', 'O', '.', 'O']
+    line1 = ['O', 'O', 'O', 'O', 'O', 'O', 'O', '.', '.', '.', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
+    line2 = ['O', '.', '.', '.', '.', '.', 'O', '.', '.', '.', 'O', '.', '.', '.', '.', '.', 'O']
+    line4 = ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
+    board = []
+    board.append(line1.copy())
+    board.append(line2.copy())
+    board.append(line3.copy())
+    board.append(line2.copy())
+    board.append(line3.copy())
+    board.append(line2.copy())
+    board.append(line1.copy())
+    board.append(line4.copy())
+    board.append(line4.copy())
+    board.append(line4.copy())
+    board.append(line1.copy())
+    board.append(line2.copy())
+    board.append(line3.copy())
+    board.append(line2.copy())
+    board.append(line3.copy())
+    board.append(line2.copy())
+    board.append(line1.copy())
+    for i in range(1, 16):
+        board[i][8] = '□'
+        board[8][i] = '□'
+    return board
 
 def move(dice, currentPlayer):
     global piece_positions
     color = currentPlayer
     piece = int(input(f"Which piece of {color} would you like to move (1-4)? ")) - 1
-    
+
     current_pos = piece_positions[color][piece]
-    path = paths[color]
-    current_index = path.index(current_pos)
-    new_index = current_index + dice
     
-    if new_index >= len(path):
-        print("Move exceeds the track length. Try a different piece or wait for next turn.")
+    try:
+        if current_pos in home_lanes[color]:
+            # If the piece is already in the home lane
+            current_index = len(track) + home_lanes[color].index(current_pos)
+        else:
+            current_index = track.index(current_pos)
+    except ValueError:
+        print("Invalid move. The piece is not on the track.")
         return
-    
-    new_pos = path[new_index]
+
+    new_index = current_index + dice
+    home_entry = track.index(starting_positions[color])  # Position on the track where home entry starts
+
+    # Check if the piece should move into the home lane
+    if current_index < home_entry and new_index >= home_entry:
+        home_move = new_index - home_entry
+        if home_move < len(home_lanes[color]):
+            new_pos = home_lanes[color][home_move]
+        else:
+            required_roll = home_entry + len(home_lanes[color]) - new_index
+            print(f"You need an exact roll of {required_roll} to enter the home lane.")
+            return
+    elif new_index >= len(track):  # Wrap around the track
+        new_index -= len(track)
+        new_pos = track[new_index]
+    else:
+        new_pos = track[new_index]
+
+    # Ensure the piece doesn't move into another player's home lane
+    if new_pos in [home for home_color in home_lanes.keys() if home_color != color for home in home_lanes[home_color]]:
+        print("You cannot move into another player's home lane.")
+        return
 
     if is_valid_move(new_pos, color):
-        board[current_pos[0]][current_pos[1]] = '.'  # Clear old position
+        board[current_pos[0]][current_pos[1]] = '.'  # Remove the piece from its current position
+        piece_positions[color][piece] = new_pos
+
         if board[new_pos[0]][new_pos[1]] == '.':
-            piece_positions[color][piece] = new_pos  # Move to new position
             board[new_pos[0]][new_pos[1]] = color[0]  # Update board with new position
         else:
-            # Check if it's an enemy piece and remove it
             enemy_color = get_piece_color(board[new_pos[0]][new_pos[1]])
-            if enemy_color != color:
+            if enemy_color == 'home_lane':
+                piece_positions[color][piece] = new_pos
+                board[new_pos[0]][new_pos[1]] = color[0]
+            elif enemy_color != color:
                 if board[new_pos[0]][new_pos[1]].islower():
-                    losePiece(enemy_color)
-                    piece_positions[color][piece] = new_pos  # Move to new position
-                    board[new_pos[0]][new_pos[1]] = color[0]  # Update board with new position
+                    losePiece(enemy_color)  # Capture the enemy piece
+                    piece_positions[color][piece] = new_pos
+                    board[new_pos[0]][new_pos[1]] = color[0]
                 else:
-                    print("Cannot move to a protected enemy's piece")
+                    print("Cannot move to a protected enemy's piece.")
+                    piece_positions[color][piece] = current_pos
+                    board[current_pos[0]][current_pos[1]] = color[0]
             else:
-                # It's a same-color piece
                 if board[new_pos[0]][new_pos[1]].islower():
                     board[new_pos[0]][new_pos[1]] = board[new_pos[0]][new_pos[1]].upper()
-                print("Your piece is now protected!")
+                    print("Your piece is now protected!")
+                else:
+                    print("Cannot move to a protected enemy's piece.")
+                    piece_positions[color][piece] = current_pos
+                    board[current_pos[0]][current_pos[1]] = color[0]
     else:
         print("Invalid move.")
 
-def calculate_new_position(current_pos, dice):
-    # For Ludo, this function is now embedded in the move logic using paths
-    pass
-
 def is_valid_move(new_pos, color):
-    # Add custom game rules for checking validity
     if new_pos[0] < 0 or new_pos[1] < 0 or new_pos[0] >= len(board) or new_pos[1] >= len(board[0]):
         return False
     return True
 
+def losePiece(color: str):
+    global players
+    if len(players[color]) > 0:
+        players[color].pop()
+        print(f'{color.capitalize()} player now has {len(players[color])} pieces left')
+
+        if len(players[color]) == 0:
+            print(f'{color.capitalize()} has been eliminated')
+            players.pop(color)
+
+        # Find and remove the captured piece from the board
+        for i in range(len(piece_positions[color])):
+            if piece_positions[color][i] not in home_lanes[color] and piece_positions[color][i] in track:
+                captured_pos = piece_positions[color][i]
+                piece_positions[color][i] = None
+                board[captured_pos[0]][captured_pos[1]] = '.'
+                break
+
 def get_piece_color(piece):
+    if piece == '□':
+        return 'home_lane'  # Return a specific indicator for home lanes
     for color in players.keys():
         if piece.lower() == color[0]:
             return color
     return None
 
-# Initialize game and board setup as before
 def main():
     global players, board, piece_positions
+    winning = None
+
     players = setPlayers()
     board = createBoard()
     currentPlayer = list(players.keys())[np.random.randint(0, len(list(players.keys())))]
+    
+    # Initialize piece positions based on starting positions
     piece_positions = {
-        'green': [(2, 2), (2, 4), (4, 2), (4, 4)],
-        'yellow': [(2, 12), (2, 14), (4, 12), (4, 14)],
-        'red': [(12, 2), (12, 4), (14, 2), (14, 4)],
-        'blue': [(12, 12), (12, 14), (14, 12), (14, 14)]
+        color: [starting_positions[color] for _ in range(4)]
+        for color in starting_positions
     }
+
     placePlayers(players, board)
     showBoard(board)
     
-    for i in range(12):  # Testing loop, can be changed to a while loop for the actual game
-        dice = np.random.randint(1, 7)
-        print(currentPlayer, 'rolled a', dice, end='! ')
-        while dice == 6:
-            print(currentPlayer, 'goes again and rolls a', end=' ')
+    while not winning:  # Testing loop, can be changed to a while loop for the actual game
+        total_dice = 0
+        while True:
             dice = np.random.randint(1, 7)
-            print(dice)
-
-        move(dice, currentPlayer)
+            total_dice += dice
+            print(currentPlayer, 'rolled a', dice, end='! ')
+            if dice != 6:
+                break
+            print(currentPlayer, 'goes again and rolls a', end=' ')
+        
+        move(total_dice, currentPlayer)
         currentPlayer = nextTurn(currentPlayer)
         print(currentPlayer, 'its your turn now')
         showBoard(board)
